1bb62083d7ca57a22234a1bc18f5e0beeb0964a2
PRITIKA1603 <pritika.j2023it@sece.ac.in>
2025-01-23 11:56:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+const express=require('express')
+const app=express()
+app.use(express.json());//parse json
+const mongoose=require('mongoose')
+const { v4: uuidv4 } = require('uuid');
+const PORT=8000
+
+const mongourl=("mongodb+srv://pritikaj2023it:pritika0916@cluster0.yqpyu.mongodb.net/")
+mongoose
+   .connect(mongourl)
+   .then(()=>{
+    console.log("DB connected")
+    app.listen(PORT,()=>{
+        console.log("my server is running")
+    })
+   })
+
+   const expenseSchema=new mongoose.Schema({
+    id:{type:String,required:true,unique:true},
+    title:{type:String,required:true},
+    amount:{type:Number,required:true}
+   });
+
+   const expenseModel=mongoose.model("expense-trackers",expenseSchema) //expense_tracker--collectionname
+
+
+   app.post("/api/expenses",async(req,res)=>{
+    const{title,amount}=req.body;
+    const newExpense=new expenseModel({
+        id:uuidv4(),
+        title:title,
+        amount:amount
+    });
+    const savedExpense=await newExpense.save();
+    res.status(200).json(savedExpense);
+   })
+
+   app.get("/api/expenses",async(req,res)=>{
+    const data=await expenseModel.find({});
+    res.json(data)
+   })
+
+   //get by id
+   app.get("/api/expensesById/:id",async(req,res)=>{
+    const{id}=req.params;
+    const expenses=await expenseModel.findOne({id});
+    res.json(expenses);
+   })
+
+   app.put("/api/expenses/:id",async(req,res) => {
+    const {id} = req.params;
+    const{title,amount} = req.body;
+    const updatedExpenses = await expenseModel.findOneAndUpdate(
+        {
+            id:id
+        },
+        {
+            title:title,
+            amount:amount,
+        }
+    )
+    res.status(200).json(updatedExpenses);
+})
+
+app.delete("/api/expenses/:id", async (req, res) => {  
+    const { id } = req.params;  
+    try {  
+        const deletedExpense = await expenseModel.findOneAndDelete({ id });  
+        
+        if (!deletedExpense) {  
+            return res.status(404).json({ message: "Expense not found" });  
+        }  
+        
+        res.status(200).json({ message: "Expense deleted successfully", deletedExpense });  
+    } catch (error) {  
+        res.status(500).json({ message: "Error deleting expense", error });  
+    }  
+});
